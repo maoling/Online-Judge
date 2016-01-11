38f7c556b7bdeaeea3939eecfdebb728cb4fa839
maoling <maoling199210191@sian.com>
2016-01-11 10:36:49 +0800

    twer

--- src/Main__1535.java
+++ src/Main__1535.java
@@ -1,5 +1,16 @@
 import java.util.*;
-
+/*
+题目描述：
+给定两个字符串，求它们前后重叠的最长子串的长度，比如"abcde"和“cdefg”是"cde"，长度为3。
+输入：
+输入可能包含多个测试案例。
+对于每个测试案例只有一行， 包含两个字符串。字符串长度不超过1000000，仅包含字符'a'-'z'。
+输出：
+对应每个测试案例，输出它们前后重叠的最长子串的长度。
+样例输入：
+abcde cdefg
+样例输出：
+ * */
 public class Main__1535 {
 	public static void main(String args[]) {
 		Scanner cin = new Scanner(System.in);
