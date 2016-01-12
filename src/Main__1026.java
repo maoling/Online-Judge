8f86bc99be9c15cdfe0027c14244a90e903e93df
maoling <maoling199210191@sian.com>
2016-01-12 14:33:41 +0800

    y6

---
+++ src/Main__1026.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main__1026 {
+	public static void main(String args[]) {
+		Scanner cin = new Scanner(System.in);
+		int m,a,b;
+		while (cin.hasNext()) {
+			m = cin.nextInt(); 
+			
+			
+		}
+	}
+}
