20692a81297a29fd634fd516bbb7747b25672ab0
maoling <maoling199210191@sian.com>
2016-01-12 17:29:59 +0800

    a

---
+++ src/Main_1099.java
@@ -0,0 +1,35 @@
+import java.util.*;
+/*
+ * 题目描述：
+对于一个字符串，将其后缀子串进行排序，例如grain
+其子串有：
+grain 
+rain 
+ain 
+in 
+n
+然后对各子串按字典顺序排序，即： 
+ain,grain,in,n,rain
+输入：
+每个案例为一行字符串。
+输出：
+将子串排序输出
+样例输入：
+grain
+样例输出：
+ain
+grain
+in
+n
+rain*/
+public class Main_1099 {
+	public static void main(String args[]) {
+		Scanner cin = new Scanner(System.in);
+		int n;
+		while (cin.hasNext()) {
+			n = cin.nextInt(); 
+			
+			
+		}
+	}
+}
