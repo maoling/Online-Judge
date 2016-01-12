48179e05cfb95bc8e1c03e90c64a6794c3e97db1
maoling <maoling199210191@sian.com>
2016-01-12 17:29:02 +0800

    a

---
+++ src/Main_template.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main_template {
+	public static void main(String args[]) {
+		Scanner cin = new Scanner(System.in);
+		int n;
+		while (cin.hasNext()) {
+			n = cin.nextInt(); 
+			
+			
+		}
+	}
+}
