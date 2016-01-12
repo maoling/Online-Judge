3c88f32892fd92e336ea36b64a56ba1ba13b2c76
maoling <maoling199210191@sian.com>
2016-01-12 18:00:47 +0800

    Z

--- src/Main_1099.java
+++ src/Main_1099.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.Map.Entry;
 /*
  * 题目描述：
 对于一个字符串，将其后缀子串进行排序，例如grain
@@ -25,11 +26,31 @@ rain*/
 public class Main_1099 {
 	public static void main(String args[]) {
 		Scanner cin = new Scanner(System.in);
-		int n;
+		String str ;	
+		
 		while (cin.hasNext()) {
-			n = cin.nextInt(); 
-			
-			
+				//Map<String, Integer> map = new HashMap<String, Integer>();
+				Map<String, Integer> map = new TreeMap<String, Integer>(
+		                new Comparator<String>() {
+		                    public int compare(String obj1, String obj2) {
+		                        // 升序排序
+		                        return obj1.compareTo(obj2);
+		                    }
+		                });
+				
+				str = cin.nextLine(); 
+				char charArray[] = str.toCharArray();
+				
+				for(int i=0;i<charArray.length;i++){				
+						String temp = str.substring(i,charArray.length);//substring 不包括endIndex位置的字符					
+						map.put(temp, 1);										
+				}
+				
+				for (Entry<String, Integer> entry : map.entrySet()) {				
+					   System.out.println(entry.getKey());				   
+				}
+				
+			}
+               						
 		}
-	}
 }
