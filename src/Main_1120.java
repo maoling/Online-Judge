036196b9b26eade426d79bb7ecbebb8d6b293bbf
maoling <maoling199210191@sina.com>
2016-01-13 13:38:04 +0800

    1-13

---
+++ src/Main_1120.java
@@ -0,0 +1,15 @@
+import java.util.*;
+/*
+ * 
+ * */
+public class Main_1120 {
+	public static void main(String args[]) {
+		Scanner cin = new Scanner(System.in);
+		int n;
+		while (cin.hasNext()) {
+			n = cin.nextInt(); 
+			
+			
+		}
+	}
+}
